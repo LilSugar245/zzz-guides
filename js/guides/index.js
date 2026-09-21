545a098c393c9fa8dbcb4db11430ae4de6934c6b
LilSugar245 <aloisetienvre5@gmail.com>
2026-09-21 22:05:08 +0200

    Update index.js

--- js/guides/index.js
+++ js/guides/index.js
@@ -2,13 +2,17 @@ import { generateTriggerGuide } from './trigger.js';
 import { generateJaneDoeGuide } from './jane.js';
 import { generateClaretGuide } from './claret.js';
 import { generateYeShunguangGuide } from './yeshunguang.js';
+import { generateYanagiGuide } from './yanagi.js';
+import { generateNangongYuGuide } from './nangongyu.js';
 import { generateGenericGuide } from './generic.js';
 
 export const guides = {
     'trigger': generateTriggerGuide,
     'jane': generateJaneDoeGuide,
     'claret': generateClaretGuide,
-    'ye shunguang': generateYeShunguangGuide
+    'ye shunguang': generateYeShunguangGuide,
+    'yanagi': generateYanagiGuide,
+    'nangong yu': generateNangongYuGuide
 };
 
 export { generateGenericGuide };
