4c75b3a51e2758fe9998b1ba3141dcae2cbd5ec6
LilSugar245 <aloisetienvre5@gmail.com>
2026-09-22 21:41:09 +0200

    Update koleda.js

--- js/guides/koleda.js
+++ js/guides/koleda.js
@@ -165,7 +165,7 @@ export function generateKoledaGuide() {
                             </div>
                         </div>
                         <div class="flex gap-4 p-3.5 rounded-xl border border-zinc-800 bg-[#0f0f0f] hover:bg-[#151515] transition-colors">
-                            <div class="w-14 h-14 bg-black rounded-lg border border-zinc-800 overflow-hidden"><img src="assets/W-Engine/W-Engine_Precious_Fossilized_Core.png" onerror="this.onerror=null; this.src='assets/W-Engine/Precious_Fossilized_Core.png';" class="w-full h-full object-cover"></div>
+                            <div class="w-14 h-14 bg-black rounded-lg border border-zinc-800 overflow-hidden"><img src="assets/W-Engine/Precious_Fossilized.png" onerror="this.onerror=null; this.src='assets/W-Engine/Precious_Fossilized.png';" class="w-full h-full object-cover"></div>
                             <div class="flex flex-col justify-center">
                                 <span class="text-[10px] text-zinc-500 uppercase font-bold mb-1">Moteur A</span>
                                 <span class="font-black text-xs uppercase text-zinc-300 tracking-wide">Precious Fossilized</span>
