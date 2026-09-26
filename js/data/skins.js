1b7e8071fa3e7c88ec2f71477aa60a8a031d1018
LilSugar245 <aloisetienvre5@gmail.com>
2026-09-26 15:56:23 +0200

    Create skins.js

---
+++ js/data/skins.js
@@ -0,0 +1,40 @@
+export const skinsData = {
+    "Remielle": [
+        { 
+            id: "3115810", 
+            name: { fr: "Éveil Blanc", en: "White Awakening" },
+            desc: { 
+                fr: "Un ange de l'ancien temps est tombé dans le monde des mortels, ses plumes dérivant alors qu'il sombrait dans un sommeil silencieux. Réveillez-vous de ce désert pâle, au-delà de la frontière de tout son, et commencez un nouveau voyage.", 
+                en: "An angel of old fell to the mortal world, their feathers drifting down as they sank into silent slumber. Awaken from the pale wasteland, beyond the edge of all sound, and begin the journey anew." 
+            },
+            img: "Remielle_3115810.png"
+        },
+        { 
+            id: "3115811", 
+            name: { fr: "Ange Déchu", en: "Fallen Angel" },
+            desc: { 
+                fr: "Puisque le soleil n'a jamais fait preuve de clémence, embrassons la chute aux côtés de la nuit au clair de lune. Vous dites qu'il est difficile de différencier les blagues de la tromperie ? Alors faisons une autre partie de notre jeu de questions-réponses, mon cher complice.", 
+                en: "Since the sun has never shown mercy, let us embrace the fall alongside the moonlit night. You say it's difficult to differentiate between jokes and deception? Then let's have another round of our Q&A game, my dear partner in crime." 
+            },
+            img: "Remielle_3115811.png"
+        },
+        { 
+            id: "3115812", 
+            name: { fr: "Pas de Deux", en: "Pas de Deux" },
+            desc: { 
+                fr: "À l'origine, c'était une danse en solo, mais un rayon de lumière a illuminé le secret et la solitude enveloppés dans les ténèbres. Maintenant, voudriez-vous vous joindre à moi pour un pas de deux, mon complice ?", 
+                en: "This was originally a solo dance, yet a ray of light has illuminated the secrecy and solitude shrouded in darkness. Now, would you care to join me for a pas de deux, my partner in crime?" 
+            },
+            img: "Remielle_3115812.png"
+        },
+        { 
+            id: "3115813", 
+            name: { fr: "Ange de l'Ombre", en: "Shadow Angel" },
+            desc: { 
+                fr: "Puisque le soleil n'a jamais fait preuve de clémence, embrassons la chute aux côtés de la nuit au clair de lune. Vous dites qu'il est difficile de différencier les blagues de la tromperie ? Alors faisons une autre partie de notre jeu de questions-réponses, mon cher complice.", 
+                en: "Since the sun has never shown mercy, let us embrace the fall alongside the moonlit night. You say it's difficult to differentiate between jokes and deception? Then let's have another round of our Q&A game, my dear partner in crime." 
+            },
+            img: "Remielle_3115813.png"
+        }
+    ]
+};
